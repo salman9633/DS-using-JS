bd888122aae989ab203a694d48bea6b3a0aff034
salmanfaris <salmanvaipees913@gmail.com>
2023-01-28 20:46:41 +0530

    sorting

---
+++ sorting/bubbleSort.ts
@@ -0,0 +1,16 @@
+function bubble(arr: number[]) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+        return arr
+    }
+}
+console.log(bubble([1, 0, 3, 7, 4]));
+
+export{}
+
